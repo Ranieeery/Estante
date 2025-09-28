95e8a94c50847cf90dcc73237e5503a6a5d03a78
Ranieeery <raniery2003@hotmail.com>
2025-09-28 18:21:29 -0300

    feat: initialize project with Spring Boot and Maven setup

---
+++ src/test/java/dev/raniery/estante/EstanteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raniery.estante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstanteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
