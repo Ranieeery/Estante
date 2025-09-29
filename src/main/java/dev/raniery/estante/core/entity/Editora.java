21fbeee119697a699a321b76d4966a1bad232ea7
Ranieeery <raniery2003@hotmail.com>
2025-09-29 13:07:48 -0300

    feat: add TipoEditora enum and integrate it into Editora entity

--- src/main/java/dev/raniery/estante/core/entity/Editora.java
+++ src/main/java/dev/raniery/estante/core/entity/Editora.java
@@ -1,22 +1,25 @@
 package dev.raniery.estante.core.entity;
 
+import dev.raniery.estante.core.enums.TipoEditora;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-@SuppressWarnings("LombokGetterMayBeUsed")
 public class Editora {
-    private Long id;
+    private final Long id;
     private String name;
     private Set<String> aliases;
     private String site;
+    private final TipoEditora publisherType;
 
-    public Editora(Long id, String name, Set<String> aliases, String site) {
+    public Editora(Long id, String name, Set<String> aliases, String site, TipoEditora publisherType) {
         this.id = id;
         setName(name);
         this.aliases = (aliases != null) ? new HashSet<>(aliases) : new HashSet<>();
         setSite(site);
+        this.publisherType = Objects.requireNonNull(publisherType, "TipoEditora cannot be null");
     }
 
     public Long getId() {
@@ -44,6 +47,10 @@ public class Editora {
         return site;
     }
 
+    public TipoEditora getPublisherType() {
+        return publisherType;
+    }
+
     public void setSite(String site) {
         this.site = (site != null) ? site.trim() : null;
     }
@@ -66,6 +73,6 @@ public class Editora {
 
     @Override
     public String toString() {
-        return "Editora{ id=" + id + ", name='" + name + '\'' + ", aliases=" + aliases + ", site='" + site + '\'' + "}";
+        return "Editora{ id=" + id + ", name='" + name + '\'' + ", aliases=" + aliases + ", site='" + site + '\'' + ", publisherType=" + publisherType + "}";
     }
 }
