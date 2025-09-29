4e66759e278170b685aced2245bca314dba71f3e
Ranieeery <raniery2003@hotmail.com>
2025-09-29 12:24:17 -0300

    feat: make Autor id field final for immutability

--- src/main/java/dev/raniery/estante/core/entity/Autor.java
+++ src/main/java/dev/raniery/estante/core/entity/Autor.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public class Autor {
 
-    private Long id;
+    private final Long id;
     private String name;
     private Set<String> aliases;
     private GeneroAutor gender;
